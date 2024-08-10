ae82b09f5664ac66b39a0ca1d5c68d61cd7844df
radhaya9av <radhaya9av@gmail.com>
2024-08-10 17:51:49 +0530

    footerStyling

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,6 +1,14 @@
+import { MdOutlinePowerSettingsNew } from "react-icons/md";
+
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="block"></div>
+      <div className="data"></div>
+      <div className="block">
+        <MdOutlinePowerSettingsNew />
+      </div>
+    </footer>
   )
 }
 
